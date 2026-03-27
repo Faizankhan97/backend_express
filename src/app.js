fe1416a9fd35b6c6af65090005c379cdfb2f0965
Faizan Khan <faizikhan753@gmail.com>
2026-03-28 00:20:48 +0500

    http method

--- src/app.js
+++ src/app.js
@@ -3,17 +3,37 @@ const express = require("express");
 const app = express();
 
 //This function is know as requried Handler
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Faizan", lastname: "Khan" });
+});
+
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  res.send("Data Saved Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted Successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data Updated Successfully");
+});
+
+// This will match all the http method API call to /test
 app.use("/test", (req, res) => {
   res.send("New Page from the Server");
 });
 
-app.use("/profile", (req, res) => {
-  res.send("This is Profile page!");
-});
+// app.use("/profile", (req, res) => {
+//   res.send("This is Profile page!");
+// });
 
-app.use("/", (req, res) => {
-  res.send("Hello from the Server");
-});
+// app.use("/", (req, res) => {
+//   res.send("Hello from the Server");
+// });
 
 app.listen(3000, () => {
   console.log("Server started");
