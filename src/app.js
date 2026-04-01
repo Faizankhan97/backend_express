e9dc5570bdde5c72eb746919036f5062d582bbc9
Faizan Khan <faizikhan753@gmail.com>
2026-04-02 01:51:13 +0500

    send post data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Aysha",
+    lastName: "Faizan",
+    emailId: "aysha@example.com",
+    password: "aysha123",
+  };
+
+  try {
+    // Creating a new instance of the User model
+    const user = new User(userObj);
+    await user.save();
+  } catch (error) {
+    res.status(400).send(error.message, "Error in creating user");
+  }
+
+  res.send("User created successfully");
+});
 
 connectDB()
   .then(() => {
